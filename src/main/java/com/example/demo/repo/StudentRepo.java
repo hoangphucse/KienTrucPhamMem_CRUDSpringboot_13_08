db77d68aa9ce44ac5d858a6e6ac0f4b2847e6d01
hoangphuc <phucherose@gmail.com>
2021-09-13 11:40:34 +0700

    set up repo

--- src/main/java/com/example/demo/repo/StudentRepo.java
+++ src/main/java/com/example/demo/repo/StudentRepo.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Student;
 
 public interface StudentRepo extends JpaRepository<Student, Integer> {
-	
+	boolean CreateStudent(Student student);
+	List<Student> getStudents();
+	boolean updateStudent(Student student);
+	boolean deleteStudent (int idStudent);
 }
