089b1b2e02900c112b43e41b4f27ef9d286541a2
hoangphuc <phucherose@gmail.com>
2021-09-13 15:07:39 +0700

    master branch

--- src/main/java/com/example/demo/repo/StudentRepo.java
+++ src/main/java/com/example/demo/repo/StudentRepo.java
@@ -5,10 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Student;
-
 public interface StudentRepo extends JpaRepository<Student, Integer> {
-	boolean CreateStudent(Student student);
-	List<Student> getStudents();
-	boolean updateStudent(Student student);
-	boolean deleteStudent (int idStudent);
+
 }
