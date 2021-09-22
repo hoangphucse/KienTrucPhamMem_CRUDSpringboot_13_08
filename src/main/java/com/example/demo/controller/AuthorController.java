8f5282a75ad08f3b3e6c335e75daf4a24bc7b8c6
hoangphuc <phucherose@gmail.com>
2021-09-22 11:14:11 +0700

    test

---
+++ src/main/java/com/example/demo/controller/AuthorController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+
+@RestController
+public class AuthorController {
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+        return userService.createUser(user);
+    }
+
+}
